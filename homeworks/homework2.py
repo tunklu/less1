d4decb4cb9b84629fca7fa78e1146cd95cc48126
Denis <vin_ogr@list.ru>
2024-05-04 21:52:40 +0700

    задание динамическая типизация

---
+++ homeworks/homework2.py
@@ -0,0 +1,10 @@
+name = 'Denis'
+print (name, type(name))
+age = 41
+print(age, type(age))
+age = age + 1
+print (age, type(age))
+age = 41.0
+print (age, type(age))
+is_student = True
+print (is_student, type(is_student))
