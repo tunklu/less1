c8803e6d322e6f4bae974ce0b6211ba1e3e42dc9
Denis <vin_ogr@list.ru>
2024-05-04 23:50:39 +0700

    задание списки и словари

---
+++ homeworks/homework5.py
@@ -0,0 +1,7 @@
+my_list = ['Blackberry', 'Raspberry', 'Cranberry', 'Cranberry', 'Cherry', 'Strawberry']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'Cowberry'
+print(my_list)
