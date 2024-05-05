a028a5d59ecba4d6e33728da3abd9b4059b4511e
Denis <vin_ogr@list.ru>
2024-05-05 22:30:56 +0700

    задание Доработка

--- homeworks/homework3.py
+++ homeworks/homework3.py
@@ -1,7 +1,34 @@
 my_string = input()
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.rstrip())
-print(my_string[0])
-print(my_string[-1])
-print(my_string[len(my_string)-1])
+
+print('Строка верхний регистр: ', my_string.upper())
+
+print('Строка нижний регистр: ', my_string.lower())
+
+print('Метод удаления пробелов стиль Python: ')
+print((''.join(my_string.split())))
+
+print('Метод удаления пробелов 2: ')
+stripless_string = ''
+for i in my_string:
+    if i != ' ':
+        stripless_string += i
+print(stripless_string)
+
+print('Метод удаления пробелов 3: ')
+stripless_string = ''
+symbol_ = ''
+i = 0
+while i != len(my_string):
+    if my_string[i] == ' ':
+        symbol_ = ''
+    else:
+        symbol_ = my_string[i]
+    stripless_string = stripless_string + symbol_
+    i = i + 1
+print(stripless_string)
+
+print('Первый элемент: ', my_string[0])
+
+print('Последний элемент стиль Python: ', my_string[-1])
+
+print('Последний элемент классический: ', my_string[len(my_string)-1])
