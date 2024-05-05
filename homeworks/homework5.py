aababe7192309bcb56cfc244f0b5fa66ed5ceb01
Denis <vin_ogr@list.ru>
2024-05-05 11:34:27 +0700

    задание Списки и словари

--- homeworks/homework5.py
+++ homeworks/homework5.py
@@ -1,7 +1,14 @@
-my_list = ['Blackberry', 'Raspberry', 'Cranberry', 'Cranberry', 'Cherry', 'Strawberry']
-print(my_list)
-print(my_list[0])
-print(my_list[-1])
-print(my_list[2:5])
+print('Работа со списками.')
+my_list = ['Blackberry', 'Raspberry', 'Cranberry', 'Cherry', 'Strawberry']
+print('List:', my_list)
+print('First:', my_list[0])
+print('Last:', my_list[-1])
+print('Sublist 3-5:', my_list[2:5])
 my_list[2] = 'Cowberry'
-print(my_list)
+print('Modified list:', my_list)
+print('Работа со словарями.')
+my_dict = {'Blackberry': 'Ежевика' , 'Raspberry': 'Малина', 'Cranberry': 'Клюква', 'Cherry': 'Вишня', 'Strawberry': 'Клубника'}
+print('Dictionary:', my_dict)
+print('Translation:', my_dict.get('Cherry'))
+my_dict['Strawberry'] = 'Земляника'
+print('Modified dictionary: ', my_dict)
