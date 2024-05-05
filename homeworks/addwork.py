cdd4ca2920e3b05189f49e9aaed9214a01df886a
Denis <vin_ogr@list.ru>
2024-05-05 13:01:33 +0700

    задание Дополнительное задание

--- homeworks/addwork.py
+++ homeworks/addwork.py
@@ -1,10 +1,8 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students_sorted = list(students)
-students_sorted.sort()
+students_sorted = sorted(list(students))
 students_average = {}
-print(students_sorted)
-for (index, item) in enumerate(students):
+for (index, item) in enumerate(students_sorted):
     sum_average = 0
     sum_average = (sum(grades[index])/(len(grades[index])))
     students_average[item] = sum_average
